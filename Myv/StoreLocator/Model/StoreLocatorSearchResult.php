fca3de5a63cdbcf5d9cdcb5b9bd63ed8729725b9
Andriy Sotnyk <53307226+Zaba-web@users.noreply.github.com>
2022-03-11 09:02:26 +0200

    remove

--- Myv/StoreLocator/Model/StoreLocatorSearchResult.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Myv\StoreLocator\Model;
-
-use Myv\StoreLocator\Api\StoreLocatorSearchResultInterface;
-use Magento\Framework\Api\SearchResults;
-
-class StoreLocatorSearchResult extends SearchResults implements StoreLocatorSearchResultInterface {
-    
-}
